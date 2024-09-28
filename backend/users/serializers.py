cf8248df248b3a711433a93bc3e0de9b3fa03958
Emirhan Tavus <emirhantavus17@gmail.com>
2024-09-28 23:47:26 +0300

    registerSerializer add

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -16,3 +16,28 @@ class UserSerializer(serializers.ModelSerializer):
       
       def get_following_count(self, obj):
         return obj.following.count()
+  
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+      password = serializers.CharField(write_only=True,required=True,style={'input_type':'password'})
+      password2 = serializers.CharField(write_only=True,required=True,style={'input_type':'password'})
+      
+      class Meta:
+            model = get_user_model()
+            fields = ['email','password','password2','first_name','last_name']
+      
+      def validate(self, attrs):
+           if attrs['password'] != attrs['password2']:
+                 raise serializers.ValidationError({'error':'passwords do not match.'})
+           return attrs
+      def create(self, validated_data):
+            validated_data.pop('password2')
+            user = get_user_model().objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
