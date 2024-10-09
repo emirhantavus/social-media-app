288d7382346220692ecdc95419cc7c069e251a0c
Emirhan Tavus <emirhantavus17@gmail.com>
2024-10-09 23:46:30 +0300

    Genre model add

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -12,3 +12,37 @@ class Category(models.Model):
 
       def __str__(self):
             return self.get_name_display()
+      
+      
+class Genre(models.Model):
+      GENRE_CHOICES = [
+            ('macera', 'Macera'),
+            ('korku', 'Korku'),
+            ('komedi', 'Komedi'),
+            ('dram', 'Dram'),
+            ('fantastik', 'Fantastik'),
+            ('gerilim', 'Gerilim'),
+            ('romantik', 'Romantik'),
+            ('aksiyon', 'Aksiyon'),
+            ('bilimkurgu', 'Bilim Kurgu'),
+            ('gizem', 'Gizem'),
+            ('belgesel', 'Belgesel'),
+            ('biyografi', 'Biyografi'),
+            ('suç', 'Suç'),
+            ('savaş', 'Savaş'),
+            ('tarih', 'Tarih'),
+            ('müzikal', 'Müzikal'),
+            ('psikolojik', 'Psikolojik'),
+            ('spor', 'Spor'),
+            ('western', 'Western'),
+            ('animasyon', 'Animasyon'),
+            ('çocuk', 'Çocuk'),
+            ('kısa', 'Kısa Film'),
+            ('sanat', 'Sanat'),
+            ('distopya', 'Distopya'),
+      ]
+    
+      name = models.CharField(max_length=50, choices=GENRE_CHOICES, unique=True)
+
+      def __str__(self):
+            return dict(self.GENRE_CHOICES).get(self.name)
