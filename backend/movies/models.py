6c9657ff8deaccbb8de73dea070bb55b5e5cb62e
Emirhan Tavus <emirhantavus17@gmail.com>
2024-10-12 23:17:48 +0300

    Update movie model

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from categories.models import Category , Genre
 
 class Movie(models.Model):
       tmdb_id = models.IntegerField(unique=True)
@@ -7,7 +8,9 @@ class Movie(models.Model):
       release_date = models.DateField(null=True, blank=True)
       poster_path = models.URLField(null=True, blank=True)
       language = models.CharField(max_length=10, default='tr-TR')
-      created_at = models.DateTimeField(auto_now_add=True)  
+      created_at = models.DateTimeField(auto_now_add=True)
+      category = models.ForeignKey(Category,on_delete=models.CASCADE)
+      genres = models.ManyToManyField(Genre)
       
       def __str__(self):
             return self.title   ### TMDB datas are used here
