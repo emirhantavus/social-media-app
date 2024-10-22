df65e91ba59dbdb635053122de79624b19335378
Emirhan Tavus <emirhantavus17@gmail.com>
2024-10-22 04:43:27 +0300

    Get top series and get by genre add

--- backend/series/urls.py
+++ backend/series/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from series.views import GetAllSeriesView , SearchSeriesByName , GetSeriesByActor , GetTopSeriesByYear
+from series.views import (GetAllSeriesView , SearchSeriesByName , 
+                          GetSeriesByActor , GetTopSeriesByYear , GetTopSeries,
+                          GetSeriesByGenreView)
 
 urlpatterns = [
     path('', GetAllSeriesView.as_view(), name='series-list'),
     path('search/', SearchSeriesByName.as_view(), name='search-series'),
     path('get-series-by-actor/', GetSeriesByActor.as_view(), name='series-actor'),
     path('get-top-series-by-year/', GetTopSeriesByYear.as_view(), name='series-year'),
+    path('get-top-series/', GetTopSeries.as_view(), name='series-top'),
+    path('get-series-by-genre/', GetSeriesByGenreView.as_view(), name='series-genre'),
 ]
--- backend/series/views.py
+++ backend/series/views.py
@@ -84,3 +84,66 @@ class GetTopSeriesByYear(APIView):
                   ]
                   return Response(selected_series,status=status.HTTP_200_OK)
             return Response({'message':'Error'})
+      
+      
+class GetTopSeries(APIView):
+      def get(self, request):
+            top_series = []
+            total_series = 100
+            series_per_page = 20
+            total_pages = total_series // series_per_page + (total_series % series_per_page)
+            sort_by = 'vote_average.desc'
+            series_rank = 1
+            
+            for page in range(1, total_pages + 1):
+                  series_url = f'https://api.themoviedb.org/3/discover/tv?api_key={api_key}&language={language}&sort_by={sort_by}&page={page}&vote_count.gte=1000'
+                  series_response = requests.get(series_url)
+                  if series_response.status_code == 200:
+                        series_response = series_response.json().get('results', [])
+                        top_series += [
+                        {
+                              'rank': series_rank + i,
+                              'title': series['name'],
+                              'original_title': series['original_name'],
+                              'vote_average': series['vote_average']
+                          }
+                        for i, series in enumerate(series_response)
+                      ]
+                        series_rank += len(series_response)
+                        if len(top_series) >= total_series:
+                              break
+
+            return Response(top_series[:total_series], status=status.HTTP_200_OK)
+      
+      
+class GetSeriesByGenreView(APIView):
+      def post(self,request):
+            genre = request.data.get('genre')
+            
+            if not genre:
+                  return Response({'message':'genre field is required'},status=status.HTTP_400_BAD_REQUEST)
+            
+            genre_url = f'https://api.themoviedb.org/3/genre/tv/list?api_key={api_key}&language={language}'
+            genre_response = requests.get(genre_url)
+            
+            if genre_response.status_code == 200:
+                  genres = genre_response.json().get('genres',[])
+                  genre_id = next((g['id'] for g in genres if g['name'].lower() == genre.lower()),None)
+
+                  if not genre_id:
+                        return Response({'message':'Genre not found'},status=status.HTTP_404_NOT_FOUND)
+                  
+                  series_url = f'https://api.themoviedb.org/3/discover/tv?api_key={api_key}&language={language}&with_genres={genre_id}'
+                  series_response = requests.get(series_url)
+                  if series_response.status_code == 200:
+                        series_response = series_response.json().get('results',[])
+                        selected_series = [
+                              {'name':series['name'],
+                               'original_name':series['original_name'],
+                              }
+                              for series in series_response
+                        ]
+                        return Response(selected_series,status=status.HTTP_200_OK)
+                  return Response({'message':'error'})
+            
+###### SONRA DEĞİŞTİRMEYİ UNUTMA. DB BULUNCA SERİES VE MOVİES VİEW KISIMLARINI DEĞİŞTİRECEĞİM.
